675bbac3508870797839b506c092beb9ca948d98
Pascal PITETTE <ppitette@gmail.com>
2025-12-03 20:45:41 +0100

    📦 NEW: Tuto Step 2

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(Request $request): Response
+    {
+        dd($request);
+
+        return $this->render('home/index.html.twig', [
+
+        ]);
+    }
+}
