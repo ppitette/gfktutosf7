6a2ef1133cb468dc0799e75f6fba6a2e04b297bd
Pascal PITETTE <ppitette@gmail.com>
2025-12-09 15:47:20 +0100

    📦 NEW: Tuto Step 6

---
+++ src/Validator/BanWord.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+final class BanWord extends Constraint
+{
+    public function __construct(
+        public string $mode = 'strict',
+        ?array $groups = null,
+        mixed $payload = null,
+        public string $message = '"{{ banword }}" est un mot interdit.',
+        public $banWords = ['spam', 'viagra']
+    ) {
+        parent::__construct([], $groups, $payload);
+    }
+}
