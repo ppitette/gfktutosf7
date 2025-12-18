defc70e6a8948c55d66dd5e1cf8bdfda415b6912
Pascal PITETTE <ppitette@gmail.com>
2025-12-18 19:28:12 +0100

    Tuto Step 20

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-// import './stimulus_bootstrap.js';
+import './stimulus_bootstrap.js';
 
 // import CanvasConfetti from 'canvas-confetti'
 
--- importmap.php
+++ importmap.php
@@ -38,4 +38,29 @@ return [
     'canvas-confetti' => [
         'version' => '1.9.4',
     ],
+    'tom-select' => [
+        'version' => '2.4.3',
+    ],
+    '@orchidjs/sifter' => [
+        'version' => '1.1.0',
+    ],
+    '@orchidjs/unicode-variants' => [
+        'version' => '1.1.2',
+    ],
+    'tom-select/dist/css/tom-select.default.min.css' => [
+        'version' => '2.4.3',
+        'type' => 'css',
+    ],
+    'tom-select/dist/css/tom-select.default.css' => [
+        'version' => '2.4.3',
+        'type' => 'css',
+    ],
+    'tom-select/dist/css/tom-select.bootstrap4.css' => [
+        'version' => '2.4.3',
+        'type' => 'css',
+    ],
+    'tom-select/dist/css/tom-select.bootstrap5.css' => [
+        'version' => '2.4.3',
+        'type' => 'css',
+    ],
 ];
